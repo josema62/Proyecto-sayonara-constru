7c081f50af70169acc77d1940b73952d8c89d41a
ArielBass <arcornejo15@alumnos.utalca.cl>
2018-05-14 12:23:20 -0400

    Solucion de error de conflicto
    Se soluciono el error en la clase BoletTest

--- test/BoletaTest.java
+++ test/BoletaTest.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import java.util.HashMap;
 import modelodedatos.Boleta;
 import modelodedatos.DetalleProducto;
@@ -40,8 +39,54 @@ public class BoletaTest {
         
         assertEquals(resultado, 15);
     }
+
+    @Test
+    public void consultaAgregarBoleta()
+    {
+       DetalleProducto detalle1 = new DetalleProducto(3,"Cereal Estrellitas 500g",2,2190,4380);
+       DetalleProducto detalle2 = new DetalleProducto(349844,"Salchichas PF",3,610,1230);
+       HashMap<String,DetalleProducto> muestras = new HashMap<>();
+       muestras.put("349844", detalle2);
+       muestras.put("3",detalle1);
+       Boleta prueba = new Boleta(5610,muestras,true,"13-05-2018","23:51",123,"marcos","1234");
+       
+       boolean resultado = this.consulta.registrarBoleta(prueba);
+       assertTrue(resultado);
+    }
     
+    @Test
+    public void existeBoletaTrue()
+    {
+        boolean resultado = this.consulta.existeBoleta(1);
+        assertTrue(resultado);
+    }
     
+    @Test
+    public void existeBoletaFalse()
+    {
+        boolean resultado = this.consulta.existeBoleta(-1);
+        assertFalse(resultado);
+    }
     
+    @Test
+    public void consultaVerificarPrecioExiste()
+    {
+       long resultado = this.consulta.verificarPrecio("3");
+       assertEquals(resultado,2190);
+    } 
+    
+    @Test
+    public void consultVerificarPrecioNoExiste()
+    {
+        long resultado = this.consulta.verificarPrecio("-2");
+        assertEquals(-1,resultado);
+    }
+    
+    @Test 
+    public void consultaDarDeBajaBoleta()
+    {
+        boolean resultado = this.consulta.darDeBajaBoleta(2);
+        assertTrue(resultado);
+    }
     
 }

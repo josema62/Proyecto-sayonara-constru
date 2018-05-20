2c7f49024ff18aeab6d2bd58a0adff0f92c1b7f1
Unknown <arcornejo15@alumnos.utalca.cl>
2018-05-20 01:21:47 -0400

    /InterfazGeneracionDeReportes: Auto stash before merge of "feature/InterfazGeneracionDeReportes" and "origin/feature/InterfazGeneracionDeReportes"
    -Merge en clase Test ReporteTest

--- test/ReporteTest.java
+++ test/ReporteTest.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -39,3 +40,50 @@ public class ReporteTest {
     }
     
 }
+=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.time.LocalDate;
+import java.time.Month;
+import modelodedatos.ReporteDeComprasProveedores;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import proyectoconstru.conexion.ConsultaReporte;
+
+/**
+ *
+ * @author Ariel
+ */
+public class ReporteTest {
+    private ConsultaReporte consulta;
+    
+    public ReporteTest()
+    {
+        this.consulta = new ConsultaReporte();
+    }
+    @Test
+    public void consultaobtenerReporteDeComprasAProveedoresNoVacio()
+    {
+        ReporteDeComprasProveedores resultado = this.consulta.obtenerReporteDeComprasAProveedores
+                                             (LocalDate.of(2018, Month.MAY, 10), 
+                                             LocalDate.of(2018, Month.MAY, 17));
+        System.out.println("Tamaño: " + resultado.obtenerDetallesDeReporte().size());
+        assertFalse(resultado.estaVacio());
+        
+    }
+    
+    @Test
+    public void consultaobtenerReporteDeComprasAProveedoresVacio()
+    {
+        ReporteDeComprasProveedores resultado = this.consulta.obtenerReporteDeComprasAProveedores
+                                             (LocalDate.of(2018, Month.MAY, 20), 
+                                             LocalDate.of(2018, Month.MAY, 20));
+        System.out.println("Tamaño: " + resultado.obtenerDetallesDeReporte().size());
+        assertTrue(resultado.estaVacio());
+    }
+    
+}
+>>>>>>> Stashed changes
